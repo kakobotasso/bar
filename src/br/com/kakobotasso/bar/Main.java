33f073042bcd49ec314b6759c61f1e5dd45abdbd
Kako Botasso <kakodev@gmail.com>
2012-11-26 20:59:24 -0200

    Associando elementos da tela ao codigo

--- src/br/com/kakobotasso/bar/Main.java
+++ src/br/com/kakobotasso/bar/Main.java
@@ -2,9 +2,17 @@ package br.com.kakobotasso.bar;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.Button;
 
 public class Main extends Activity {
+	Button btAdmHome;
+	Button btCarrinhoHome;
+	Button btFecharPedidoHome;
+	Intent itAdm;
+	Intent itCarrinho;
+	Intent itFecharPedido;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,4 +25,13 @@ public class Main extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    
+    // ASSOCIAR ELEMENTOS
+    public void associarElementos(){
+    	// BOTOES
+    	this.btAdmHome = (Button) findViewById(R.id.btAdmHome);
+    	this.btCarrinhoHome = (Button) findViewById(R.id.btCarrinhoHome);
+    	this.btFecharPedidoHome = (Button) findViewById(R.id.btFecharPedidoHome);
+    }
+    
 }
