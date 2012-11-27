a93fd587c9f237fb01d0fff133e7ef33571e6464
Kako Botasso <kakodev@gmail.com>
2012-11-26 22:18:36 -0200

    Iniciando criacao de layout do cadastro de cervejas

--- gen/br/com/kakobotasso/bar/R.java
+++ gen/br/com/kakobotasso/bar/R.java
@@ -15,10 +15,13 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int btAdmHome=0x7f070000;
-        public static final int btCarrinhoHome=0x7f070001;
-        public static final int btFecharPedidoHome=0x7f070002;
-        public static final int menu_settings=0x7f070003;
+        public static final int btAdmHome=0x7f070003;
+        public static final int btCadastroPais=0x7f070001;
+        public static final int btCarrinhoHome=0x7f070004;
+        public static final int btFecharPedidoHome=0x7f070005;
+        public static final int btVoltar=0x7f070002;
+        public static final int linearLayout1=0x7f070000;
+        public static final int menu_settings=0x7f070006;
     }
     public static final class layout {
         public static final int adm=0x7f030000;
@@ -32,8 +35,10 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int btAdmHome=0x7f040004;
+        public static final int btCadastroPais=0x7f040007;
         public static final int btCarrinhoHome=0x7f040005;
         public static final int btFecharPedidoHome=0x7f040006;
+        public static final int btVoltarCadastroCerveja=0x7f040008;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_main=0x7f040003;
